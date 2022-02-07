fd2a1fdc21edba8cc5c75a3ebb9a45638202d181
Rathnana <48769822+Rathnana@users.noreply.github.com>
2022-02-07 18:23:05 +0700

    Add files via upload

---
+++ src/components/all_Info/WorkerGraph.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+import { Col } from 'antd';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'ចំនួនកម្មករប្រចាំថ្ងៃ',
+        },
+    },
+};
+
+const labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'
+    , '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '27', '29', '30', '31'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+export default function WorkerGraph() {
+    return <Col style={{ marginBottom: "200px" }} xs={24} sm={24} md={12} lg={12} xl={12}>
+        <Line options={options} data={data} />
+    </Col>
+}
